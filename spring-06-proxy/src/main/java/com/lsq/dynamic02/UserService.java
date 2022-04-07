5cb4ff6d8a3bbd4a7d33741f75aa5ff269cf1339
Tame-complexity <linshengqian@outlook.com>
2022-04-07 17:04:06 +0800

    spring-proxy

---
+++ spring-06-proxy/src/main/java/com/lsq/dynamic02/UserService.java
@@ -0,0 +1,11 @@
+package com.lsq.dynamic02;
+
+/**
+ * @author linshengqian
+ */
+public interface UserService {
+    void add();
+    void delete();
+    void update();
+    void select();
+}
---
+++ spring-06-proxy/src/main/java/com/lsq/dynamic02/UserServiceProxy.java
@@ -0,0 +1,39 @@
+package com.lsq.dynamic02;
+
+/**
+ * @author linshengqian
+ */
+public class UserServiceProxy implements UserService {
+    private UserServiceImpl userService;
+
+    public UserServiceProxy(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void add() {
+        log("add");
+        userService.add();
+    }
+
+    @Override
+    public void delete() {
+        log("delete");
+        userService.delete();
+    }
+
+    @Override
+    public void update() {
+    log("upadte");
+    userService.update();
+    }
+
+    @Override
+    public void select() {
+        log("select");
+        userService.select();
+    }
+    public void log(String msg){
+        System.out.println("执行了" + msg + "方法");
+    }
+}
